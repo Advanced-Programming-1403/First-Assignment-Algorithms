4b69c65cbbf61cb5cde0b99a6405d64d0ae141e2
Hossein Rahmanzadeh <hossein@Hosseins-MacBook-Air.local>
2025-03-07 20:12:07 +0330

    done

--- AP1403 - Algorithms/src/main/java/Exercises.java
+++ AP1403 - Algorithms/src/main/java/Exercises.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Exercises {
 
     /*
@@ -9,10 +11,15 @@ public class Exercises {
         note: you should return the indices in ascending order and every array's solution is unique
     */
     public int[] productIndices(int[] values, int target) {
-        // todo
-        return null;
+        for (int i = 0; i < values.length; i++) {
+            for (int j = i + 1; j < values.length; j++) {
+                if (values[i] * values[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
     }
-
     /*
         given a matrix of random integers, you should do spiral traversal in it
         e.g. if the matrix is as shown below:
@@ -25,8 +32,40 @@ public class Exercises {
         so you should walk in that matrix in a curl and then add the numbers in order you've seen them in a 1D array
     */
     public int[] spiralTraversal(int[][] values, int rows, int cols) {
-        // todo
-        return null;
+        int[] n = new int[rows * cols];
+        int x = 0;
+        int c = 0, r = cols - 1, b = 0, p = rows - 1;
+
+        while (c <= r && b <= p) {
+            // Traverse from left to right
+            for (int i = c; i <= r; i++) {
+                n[x++] = values[b][i];
+            }
+            b++;
+
+            // Traverse from top to bottom
+            for (int i = b; i <= p; i++) {
+                n[x++] = values[i][r];
+            }
+            r--;
+
+            if (b <= p) {
+                // Traverse from right to left
+                for (int i = r; i >= c; i--) {
+                    n[x++] = values[p][i];
+                }
+                p--;
+            }
+
+            if (c <= r) {
+                // Traverse from bottom to top
+                for (int i = p; i >= b; i--) {
+                    n[x++] = values[i][c];
+                }
+                c++;
+            }
+        }
+        return n;
     }
 
     /*
@@ -54,11 +93,31 @@ public class Exercises {
         if you're familiar with lists and arraylists, you can also edit method's body to use them instead of array
     */
     public int[][] intPartitions(int n) {
-        // todo
-        return null;
+        List<List<Integer>> r = new ArrayList<>();
+        generatePartitions(n, n, new ArrayList<>(), r);
+
+        int[][] p = new int[r.size()][];
+        for (int i = 0; i < r.size(); i++) {
+            p[i] = r.get(i).stream().mapToInt(Integer::intValue).toArray();
+        }
+        return p;
     }
+    private void generatePartitions(int n, int max, List<Integer> current, List<List<Integer>> r) {
+        if (n == 0) {
+            r.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = Math.min(max, n); i >= 1; i--) {
+            current.add(i);
+            generatePartitions(n - i, i, current, r);
+            current.remove(current.size() - 1);
+        }
+    }
+
+
 
     public static void main(String[] args) {
         // you can test your code here
     }
+
 }
